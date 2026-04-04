852b7d94ded097e101fc53e3d795ff084b69a7c0
subin9 <subin8636@naver.com>
2026-04-04 17:55:37 +0900

    fix: add nvim install to install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ mkdir -p ~/.config
 # starship 설치
 curl -sS https://starship.rs/install.sh | sh
 
+# nvim 설치
+sudo snap install nvim --classic
+
 # symlink
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/starship.toml" ~/.config/starship.toml
