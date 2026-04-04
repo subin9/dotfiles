97941294376a055ac1f28fa441bddaf2416d386d
subin9 <subin8636@naver.com>
2026-04-04 18:10:23 +0900

    feat: install basic tools

--- install.sh
+++ install.sh
@@ -9,9 +9,13 @@ curl -sS https://starship.rs/install.sh | sh
 # nvim 설치
 sudo snap install nvim --classic
 
+# Install Basic Packages
+sudo apt install -y git curl wget unzip build-essential ripgrep fd-find
+
 # symlink
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/starship.toml" ~/.config/starship.toml
 ln -sf "$DOTFILES/bashrc" ~/.bashrc
 
 echo "Done!"
+ln -sf "$DOTFILES/gitconfig" ~/.gitconfig
