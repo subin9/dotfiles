70dea47cf185e2c773224bea55a6df4aa5059970
subin9 <subin8636@naver.com>
2026-04-04 17:51:09 +0900

    fix: add starship install to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 set -e
 DOTFILES="$HOME/.dotfiles"
 mkdir -p ~/.config
+
+# starship 설치
+curl -sS https://starship.rs/install.sh | sh
+
+# symlink
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/starship.toml" ~/.config/starship.toml
 ln -sf "$DOTFILES/bashrc" ~/.bashrc
+
 echo "Done!"
